13cfdcf5590d98a642077ca5079d1697568628f8
Peter Tolkachev <ptolkachev@hse.ru>
2022-04-20 21:34:09 +0300

    fix dev regime

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
+const Handlebars = require("handlebars");
+const template = Handlebars.compile("Name: {{name}}");
+
 const chat = document.getElementById("chat");
 chat.textContent = "Chat project is starting";
