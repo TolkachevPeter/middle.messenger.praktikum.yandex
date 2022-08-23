4413e124dae9fe463bb46e34a4a4c1439218315b
Peter Tolkachev <ptolkachev@hse.ru>
2022-08-23 12:54:45 +0300

    use router

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,22 @@
-import { navigateTo } from './router';
+// import router from './router';
 
-navigateTo('loginPage');
+// router;
+
+import { generateChatPage } from './pages/chats';
+// import { generateErrorPage } from '../pages/errors';
+import {generateLoginModule} from './pages/login';
+import { generateRegistrationModule } from './pages/registration';
+import { generateUserPage } from './pages/user';
+import Router from './services/router';
+
+
+const router = new Router('.chat');
+
+router
+	.use('/', new generateLoginModule())
+	.use('/sign-up', new generateRegistrationModule())
+	.use('/settings', new generateUserPage())
+	.use('/messenger', new generateChatPage())
+//   .use('/404', new generateErrorPage(404, 'Sorry, but this page does not exist'))
+//   .use('/500', new generateErrorPage(500, 'We are working to fix the problem!'))
+	.start();
