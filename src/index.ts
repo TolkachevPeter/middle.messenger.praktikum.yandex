f69f687fda5697ed8e5163b4eecfd7d01a1f07cc
Peter Tolkachev <ptolkachev@hse.ru>
2022-11-06 11:19:27 +0300

    fix error

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@
 // router;
 
 import { generateChatPage } from './pages/chats';
-// import { generateErrorPage } from '../pages/errors';
+import { generateErrorPage } from './pages/errors';
 import {generateLoginModule} from './pages/login';
 import { generateRegistrationModule } from './pages/registration';
 import { generateUserPage } from './pages/user';
@@ -17,6 +17,6 @@ router
 	.use('/sign-up', new generateRegistrationModule())
 	.use('/settings', new generateUserPage())
 	.use('/messenger', new generateChatPage())
-//   .use('/404', new generateErrorPage(404, 'Sorry, but this page does not exist'))
-//   .use('/500', new generateErrorPage(500, 'We are working to fix the problem!'))
+	.use('/404', new generateErrorPage({code: 404, description: 'Sorry, but this page does not exist'}))
+	.use('/500', new generateErrorPage({code: 500, description: 'We are working to fix the problem!'}))
 	.start();
--- src/pages/errors/error.ts
+++ src/pages/errors/error.ts
@@ -1,11 +1,53 @@
-import Handlebars from 'handlebars';
 import error from './error.tmpl';
 import './error.less';
+import Block from '../../commonClasses/Block';
+import Button from '../../components/button';
+import Router from '../../services/router';
+import RenderHelper from '../../commonClasses/RenderHelper';
 
-const template = Handlebars.compile(error);
+type ErrorProps = {
+	code: number;
+	description: string;
+}
 
-export default (errorCode, errorDescription) =>
-	template({
-		errorCode,
-		errorDescription,
-	});
+export default class Error extends Block {
+	toChat: Button;
+	router: Router;
+
+	constructor(props: ErrorProps) {
+		super('div', props, true);
+		this.router = new Router();
+	}
+
+	componentDidMount() {
+		this.toChat = new Button({
+			buttonStyle: 'defaultButton',
+			buttonText: 'Return to Chat',
+			events: {
+				click: this.onClickChat.bind(this),
+			},
+		});
+
+	}
+
+	onClickChat() {
+		this.router.go('/messenger');
+	}
+
+
+	render() {
+		const renderHelper = new RenderHelper();
+		renderHelper.registerPartial(
+			'linkToChats',
+			this.toChat.renderAsHTMLString()
+		);
+		const templateHTML = renderHelper.generate(error, {
+			code: this.props.code,
+			description: this.props.description
+		});
+		return renderHelper.replaceElements(templateHTML, [
+			this.toChat
+		]);
+	}
+}
+
