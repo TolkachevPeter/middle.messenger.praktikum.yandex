cd5051e5848b0d122810c1dc449c41640d31fc9e
TolkachevPeter <peter.tolkachev@gmail.com>
2023-06-30 21:25:06 +0300

    check: check husky

--- husky.config.js
+++ husky.config.js
@@ -2,7 +2,7 @@ const tasks = (arr) => arr.join(' && ');
 
 module.exports = {
 	hooks: {
-		'pre-commit': tasks(['lint']),
+		'pre-commit': tasks(['npm run lint']),
 		'pre-push': tasks(['npm run test']),
 		'commit-msg': 'commitlint -e $HUSKY_GIT_PARAMS',
 	},
